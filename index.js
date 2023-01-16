612cf9e021a05f130cbc184b3fd6a6c3fcb72a40
Yuvraj1307 <112754852+Yuvraj1307@users.noreply.github.com>
2023-01-16 19:48:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const express=require("express")
+const {connection}=require("./config/db")
+const {Usermodel}=require("./model/usermodel")
+const {Postmodel}=require("./model/postmodel");
+const cors = require("cors")
+
+//const jwt=require("jsonwebtoken")
+const {userRouts}=require("./routs/routs")
+const {postRout}=require("./routs/postrout")
+//const bcrypt=require("bcrypt")
+const app=express()
+app.use(express.json())
+
+app.use(cors(
+    {
+        origin:"*"
+    }
+))
+app.get("/",(req,res)=>{
+    res.send("welcome")
+})
+app.use("/user",userRouts)
+app.use("/post",postRout)
+    
+    
+ 
+
+require('dotenv').config()
+app.listen(process.env.port,async()=>{
+
+    try{
+        await connection
+        console.log("connected to database")
+    }catch(err){
+        console.log("can't connect database")
+        console.log(err)
+    }
+})
+
+
+
+
+
+
+
+// {
+//     "name" : "String",
+//   "email":"String",
+//   "gender": "String",
+//   "password" :"String"
+//   }
+
+
+
+
+
+// {
+//     "email":"String",
+//     "password" :"String"
+//     }
